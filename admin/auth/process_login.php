bde120e396d4c5f69693385e26cbbc7f3162894b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-02-06 11:19:07 -0600

    update

--- admin/auth/process_login.php
+++ admin/auth/process_login.php
@@ -78,17 +78,17 @@ try {
 
             $mail = new PHPMailer(true);
             try {
-                //Server settings
+                // SMTP settings
                 $mail->isSMTP();
-                $mail->Host       = 'smtp.gmail.com'; // Your SMTP
+                $mail->Host       = 'smtp.gmail.com';
                 $mail->SMTPAuth   = true;
                 $mail->Username   = 'garrett.morgan.pro@gmail.com';
                 $mail->Password   = '***REMOVED-SMTP-PASSWORD***';
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                 $mail->Port       = 587;
 
-                //Recipients
-                $mail->setFrom('admin@themorganlegacy.com', 'Morgan Legacy Admin');
+                // Recipients
+                $mail->setFrom('noreply@morganserver.com', 'TheMorganLegacy');
                 $mail->addAddress($admin['email']);
 
                 // Content
@@ -105,7 +105,7 @@ try {
                     <a href='$unlockLink'>$unlockLink</a>
                 ";
                 $mail->send();
-                
+
             } catch (Exception $e) {
                 error_log("Mail could not be sent: {$mail->ErrorInfo}");
             }
