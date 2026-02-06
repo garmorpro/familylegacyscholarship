30ce923d65e28e6fbf3513d188f6a04fa8915492
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-02-05 22:21:59 -0600

    update

--- admin/settings.php
+++ admin/settings.php
@@ -147,6 +147,13 @@ try {
 
 </div>
 
+<div class="alert alert-success d-flex align-items-center" role="alert">
+  <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+  <div>
+    An example success alert with an icon
+  </div>
+</div>
+
 
 
 
