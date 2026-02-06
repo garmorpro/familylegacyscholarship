159943ba8eb858416cb385cb23364e45d895ebc5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-02-06 11:54:47 -0600

    update

--- admin/index.php
+++ admin/index.php
@@ -148,7 +148,7 @@ try {
 
     <!-- Right: Settings Icon -->
      <div class="" style="display: flex;">
-<a href="<?= BASE_URL ?>/admin/auth/logout.php" title="Logout" style="color: #495057; font-size: 1.25rem; display: flex; align-items: center;">
+<a href="<?= BASE_URL ?>/admin/auth/logout.php" class="me-1" title="Logout" style="color: #495057; font-size: 1.25rem; display: flex; align-items: center;">
         <i class="bi bi-box-arrow-in-left"></i>
     </a>
     <a href="settings.php" title="Settings" style="color: #495057; font-size: 1.25rem; display: flex; align-items: center;">
