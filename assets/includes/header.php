aa0c410e504bcfd7e65de0175895f9532c455780
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-02-06 13:13:51 -0600

    update

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -6,7 +6,7 @@
       <div class="d-flex flex-column w-100">
         <!-- Top row: h1 + hamburger -->
         <div class="d-flex align-items-center justify-content-between w-100">
-            <img src="<?= BASE_URL ?>/assets/images/logo.png" alt="">
+            <img src="<?= BASE_URL ?>/assets/images/logo.png" alt="" style="height:80px;">
           <h1 class="h4 fw-semibold mb-1">
             The Morgan Family Legacy Scholarship
           </h1>
