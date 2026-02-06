0686ea6f72d965a3f54e773c46193147169605d8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-02-06 12:00:58 -0600

    update

--- admin/send_not_sent_recommendations.php
+++ admin/send_not_sent_recommendations.php
@@ -1,4 +1,11 @@
 <?php
+
+if ($_SERVER['REMOTE_ADDR'] !== '127.0.0.1') {
+    http_response_code(403);
+    exit('Forbidden');
+}
+
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
