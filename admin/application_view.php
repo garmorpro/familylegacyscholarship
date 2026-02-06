18d0054247833de9477091fca6f508c4b6dec7ef
Garrett Morgan <garrett.morgan.pro@gmail.com>
2026-02-06 15:20:45 -0600

    update

--- admin/application_view.php
+++ admin/application_view.php
@@ -207,7 +207,7 @@ try {
                     'submitted' => 'bg-primary-subtle text-primary',
                     'reviewed'  => 'bg-secondary-subtle text-secondary',
                     'final_review'  => 'bg-success-subtle text-success',
-                    'final_selected'  => 'bg-success-subtle text-success',
+                    'final_recipient'  => 'bg-success-subtle text-success',
                     default     => 'bg-light text-dark'
                 };
             ?>">
